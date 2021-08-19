2bf57ea6afb24771ea60397e7d006826e39099ab
Shahad161 <shahad.hamza.014@gmail.com>
2021-08-19 21:09:10 +0300

    add Encapsulate

--- app/src/main/java/com/example/bindingadapter/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/bindingadapter/viewModels/MainViewModel.kt
@@ -1,16 +1,19 @@
 package com.example.bindingadapter.viewModels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
 class MainViewModel: ViewModel() {
-    var myLiveData = MutableLiveData<String>()
+    private var _myLiveData = MutableLiveData("Hi")
+    val myLiveData: LiveData<String>
+            get() = _myLiveData
 
     init{
         flow {
-            emit(myLiveData.value)
+            emit(_myLiveData.value)
             delay(2000)
         }
     }
